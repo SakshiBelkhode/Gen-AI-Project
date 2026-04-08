ced7da4bd18207f4ed45d054bdb74fa510d794f3
SakshiBelkhode <126399972+SakshiBelkhode@users.noreply.github.com>
2026-04-08 15:53:13 +0530

    Started Gen-AI project

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -8,6 +8,28 @@ const authController = require("../controllers/auth.controller")
  */
 
 async function registerUserController(req, res) {
+  
+  const { userbname, email, password } = req.body
+   
+  if (!username || !email || !password) {
+    return res.status(400).json({
+        message: "Please provide username, email and password"
+    })
+   }
+  
+  const isUserAlreadyExists = await userModel.findOne({
+    $or: [{username}, {email}]
+  })
+
+  if (isUserAlreadyExists) {
+
+    /* isUser AlreadyExists.username == username */
+    return res.status(400).json ({
+        message: "Account already exists  with this email address or username"
+    })
+  }
+
+  const newUser = new userModel
 
 }
 
