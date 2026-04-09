c5144130a9be8fc8986ec37e9943e997d6d69c4e
SakshiBelkhode <126399972+SakshiBelkhode@users.noreply.github.com>
2026-04-09 21:13:45 +0530

    Started Gen-AI project

---
+++ src/models/blacklist.model.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+
+const blacklistTokenSchema = new mongoose.Schema({
+    token:{
+        type:String,
+        required:[true,"token is required to be added in blacklist"]
+    }
+    
+})
