18b16211fcf897250ea7fb120455d74ffb714d50
SakshiBelkhode <126399972+SakshiBelkhode@users.noreply.github.com>
2026-04-10 18:38:14 +0530

    Started Gen-AI project

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -105,7 +105,9 @@ async function loginUserController(req, res) {
 }
 
 /**
- * @name logUserController
+ * @name logUserController 
+ * @description clear token from user cookie and add the token in blacklist
+ * @access public
  */
 async function logoutUserController(req,res) {
     const token = req.cookies.token
@@ -125,9 +127,30 @@ async function logoutUserController(req,res) {
 }
 
 
+/**
+ * @name getMeController
+ * @description get the current logged in user details, expects token in the request
+ * @access private
+ */
+
+async function getMeController(req, res){
+
+  const user = await userModel.findById(req.user.id)
+
+  res.status(200).json({
+    message: "User details fetched successfully",
+    user:{
+      id: user._id,
+      username: user.username,
+      email: user.email
+    }
+  })
+}
+
 
 module.exports = {
   registerUserController,
   loginUserController,
-  logoutUserController
+  logoutUserController,
+  getMeController
 };
